82ad81c69fce450219b77ea91656ce727c239944
Aadi Sarma <aadisarma@Aadis-MBP.hsd1.ca.comcast.net>
2024-07-24 10:35:19 -0700

    fixed lint issues

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient
 const fs = require('fs')
+const path = require('path')
 
 // MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`
-let filename = `${__dirname}/secondChanceItems.json`
+const url = `${process.env.MONGO_URL}`
+const filename = path.join(__dirname, 'secondChanceItems.json')
 const dbName = 'secondChance'
 const collectionName = 'secondChanceItems'
 
@@ -12,7 +13,7 @@ const collectionName = 'secondChanceItems'
 const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs
 
 // connect to database and insert data into the collection
-async function loadData() {
+async function loadData () {
   const client = new MongoClient(url)
 
   try {
@@ -25,8 +26,8 @@ async function loadData() {
 
     // collection will be created if it does not exist
     const collection = db.collection(collectionName)
-    let cursor = await collection.find({})
-    let documents = await cursor.toArray()
+    const cursor = await collection.find({})
+    const documents = await cursor.toArray()
 
     if (documents.length === 0) {
       // Insert data into the collection
